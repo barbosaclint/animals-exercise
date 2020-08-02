3ad73b4f6f38f5b2ffa9a79916e719c592b553be
clint_barbosa <barbosaclint@gmail.com>
2020-08-02 19:19:46 +0800

    Initial commit animal exercise project

---
+++ src/test/java/com/clint/animals/exercise/animalsexercise/AnimalsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clint.animals.exercise.animalsexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
